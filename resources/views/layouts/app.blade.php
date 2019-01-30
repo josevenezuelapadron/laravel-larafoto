270875f03a23b9fafa97aa144bf808362f197953
José Padrón <jose.padron712@gmail.com>
2019-01-29 20:14:41 -0400

    Avatar en el navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,6 +56,10 @@
             <li class="nav-item">
               <a href="" class="nav-link">Subir imagen</a>
             </li>
+
+            <li class="nav-item">
+              @include('includes.avatar')
+            </li>
             
             <li class="nav-item dropdown">
               <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
