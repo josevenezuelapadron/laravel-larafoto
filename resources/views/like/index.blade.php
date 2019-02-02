617e4df1307a9270b0ac8d22c711f21dc732d3bc
José Padrón <jose.padron712@gmail.com>
2019-02-01 22:00:41 -0400

    Se listan todas las imagenes al cual el usuario le ha dado like

--- resources/views/like/index.blade.php
+++ resources/views/like/index.blade.php
@@ -4,15 +4,16 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @include('includes.message')
+            <h1>Mis imagenes favoritas</h1>
 
-            @foreach($images as $image)
-              @include('includes.image', ['image' => $image])
+            @foreach($likes as $like)
+              @include('includes.image', ['image' => $like->image])
             @endforeach
+
             <!-- Paginación -->
             <div class="clearfix"></div>
 
-            {{ $images->links() }}
+            {{ $likes->links() }}
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,4 @@ Route::get("/image/{id}", "ImageController@detail")->name("image.detail");
 Route::get("/comment/delete/{id}", "CommentController@delete")->name("comment.delete");
 Route::get("/like/{image_id}", "LikeController@like")->name("like.save");
 Route::get("/dislike/{image_id}", "LikeController@dislike")->name("like.delete");
+Route::get("/likes", "LikeController@index")->name("likes");
